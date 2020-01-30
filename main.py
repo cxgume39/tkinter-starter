1a14b511f350c4d4009d15cec07cdd5d68a32d54
Cornelio Gumecindo-Baltazar <cxgume39>
2020-01-30 11:17:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # A starter program for Python with Tkinter
 
 from tkinter import * # import Tkinter library
-from tkinter.ttk import Progressbar
-from tkinter import Menu
-from tkinter import ttk
-import time
-window = Tk()         # Create the application window
+# from tkinter.ttk import Progressbar
+# from tkinter import Menu
+from tkinter.ttk import
+from ttkthemes import ThemedTk
+window = ThemedTk(theme="arc")         # Create the application window
 
 # Add a label with the text "Hey"
 lbl = Label(window, text="Hello", font=("Arial Bold", 50))
@@ -25,26 +25,22 @@ chk = Checkbutton(window, text='Choose', var=chk_state)
 chk.grid(column=4, row=0)
 btn.grid(column=5, row=0)
 
-style = ttk.Style() 
-style.theme_use('default')
-style.configure("black.Horizontal.TProgressbar", background='black') 
-bar = Progressbar(window, length=200, style='black.Horizontal.TProgressbar') 
-bar['value'] = 70 
-bar.grid(column=6, row=1)
+# style = ttk.Style() 
+# style.theme_use('default')
+# style.configure("black.Horizontal.TProgressbar", background='black') 
+# bar = Progressbar(window, length=200, style='black.Horizontal.TProgressbar') 
+# bar['value'] = 70 
+# bar.grid(column=6, row=1)
 
-menu = Menu(window) 
-new_item = Menu(menu) 
-new_item.add_command(label='New')
-menu.add_cascade(label='File', menu=new_item)
-window.config(menu=menu)
+# menu = Menu(window) 
+# new_item = Menu(menu) 
+# new_item.add_command(label='New')
+# menu.add_cascade(label='File', menu=new_item)
+# window.config(menu=menu)
 
-tk=Tk()
-def clock():
-    t=time.strftime('%I:%M:%S',time.localtime())
-    if t!='':
-        label1.config(text=t,font='times 25')
-    tk.after(100,clock)
-label1=Label(tk,justify='center')
-label1.pack()
-clock()
+spin = Spinbox(window, from_=0, to=100, width=4) 
+spin.grid(column=4,row=4)
+
+
+
 window.mainloop()     # Keep the window open
